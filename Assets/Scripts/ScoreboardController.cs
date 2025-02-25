1b626fc21c9255913ba45a6ec0e340621645444d
MrHiTech123 <mlebowitz97@gmail.com>
2025-02-24 21:54:15 -0500

    Added win condition

--- Assets/Scripts/ScoreboardController.cs
+++ Assets/Scripts/ScoreboardController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreboardController : MonoBehaviour
@@ -9,14 +10,41 @@ public class ScoreboardController : MonoBehaviour
     // Start is called before the first frame update
 	
 	public int score = 0;
+	public int requiredScore = 10;
+	public int secondsNeededToWin = 5;
+	public string nextLevelSceneName;
+	private float timeOfWin;
+	private bool won = false;
+	
+	
     void Start()
     {
-        
     }
-
+	
+	void CheckIfWon() {
+		if (!won) {
+			if (score >= requiredScore) {
+				won = true;
+				timeOfWin = Time.time;
+			}
+		}
+		else if (Time.time - timeOfWin > secondsNeededToWin) {
+			SceneManager.LoadScene(nextLevelSceneName, LoadSceneMode.Single);
+		}
+		else if (score < requiredScore) {
+			won = false;
+			timeOfWin = -1;
+		}
+	}
+	void PrintScore() {
+		GetComponent<TextMeshProUGUI>().text = "Score: " + score;
+	}
+	
+	
     // Update is called once per frame
     void Update()
     {
-        GetComponent<TextMeshProUGUI>().text = "Score: " + score;
+        CheckIfWon();
+		PrintScore();
     }
 }
